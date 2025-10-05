ab827fd9c8edbed849833def8efd35ea56b57edd
SATAGOPAM SAI HARISH <22a51a4207@adityatekkali.edu.in>
2025-10-05 22:07:08 +0530

    vbhfb

--- frontend/interview-prep-ai/src/main.jsx
+++ frontend/interview-prep-ai/src/main.jsx
@@ -3,12 +3,22 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import * as tf from '@tensorflow/tfjs'
+import '@tensorflow/tfjs-backend-webgl'
 
-createRoot(document.getElementById('root')).render(
- 
- 
-      <App />
-   
+async function bootstrap() {
+  try {
+    await tf.setBackend('webgl')
+  } catch (_) {
+    try { await tf.setBackend('cpu') } catch (_) {}
+  }
+  await tf.ready()
+  createRoot(document.getElementById('root')).render(
+    
+        <App />
+      
+  )
+}
 
-)
+bootstrap()
 
